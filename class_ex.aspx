c1e7c2cd52ae008eb6ddc98c805088ecb241e191
pavan ajugiya <pavanajugiya17@gmail.com>
2026-09-09 09:54:21 +0530

    Add files via upload

---
+++ class_ex.aspx
@@ -0,0 +1,33 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="class_ex.aspx.cs" Inherits="WebApplication2.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+
+            <br />
+            Name :
+            <asp:TextBox ID="txtname" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            Age :
+            <asp:TextBox ID="txtage" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            <br />
+            <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Button" />
+            <br />
+            <br />
+            <br />
+            <br />
+            <asp:Label ID="Label1" runat="server"></asp:Label>
+
+        </div>
+    </form>
+</body>
+</html>
---
+++ class_ex.aspx.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication2
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        public class student
+        {
+            public string name;
+            public int Age;
+
+            public student()
+            {
+                name = "pavan";
+                Age = 21;
+            }
+
+            public student(string name, int age)
+            {
+                this.name = name;
+                Age = age;
+            }
+
+            public string Display()
+            {
+                return "Name : " + name + "<br/>" + "Age : " + Age + "<br/>";
+            }
+        }
+
+        
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            //student s = new student();
+            //s.name = "pavan";
+            //s.Age = 21;
+            //student s1 = new student();
+            //s1.name = "harpal";
+            //s1.Age = 30;
+            //Label1.Text = s.Display() + s1.Display();
+
+            student s = new student(s.name,s.Age);
+            s.name = txtname.Text;
+            s.Age = Convert.ToInt32(txtage.Text);
+            Label1.Text = s.Display();
+
+
+
+        }
+    }
+}
