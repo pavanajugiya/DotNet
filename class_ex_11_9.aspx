49db2116e6d38e7d1e7ed61c289a258cef688816
pavan ajugiya <pavanajugiya17@gmail.com>
2026-09-11 14:02:00 +0530

    Add files via upload

---
+++ class_ex_11_9.aspx
@@ -0,0 +1,35 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="WebApplication1.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            Person_id:-<asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            Person_name:-<asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            Student_id:-<asp:TextBox ID="TextBox3" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            Student_name:-<asp:TextBox ID="TextBox4" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            <br />
+            <br />
+            <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Button" />
+            <br />
+            <br />
+            <br />
+            <br />
+            <asp:TextBox ID="TextBox5" runat="server" Height="324px" ReadOnly="True" TextMode="MultiLine"></asp:TextBox>
+        </div>
+    </form>
+</body>
+</html>
---
+++ class_ex_11_9.aspx.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        static int count = 0;
+        public class Person
+        {
+            int Person_id;
+
+            string Name;
+            public Person(int Person_id, string Name)
+            {
+                this.Person_id= Person_id;
+                this.Name= Name;
+
+
+            }
+            public string Display_Person()
+            {
+                count++;
+                return count + ".Details \nPerson ID:-"+this.Person_id+"\nName:-"+Name;
+            }
+        }
+        public class Student : Person
+        {
+            int En;
+
+            string Name;
+            public Student(int Person_id, string P_Name,int En, string Name):base(Person_id,P_Name)
+            {
+                this.En = En;
+                this.Name = Name;
+                
+
+
+            }
+            public string Display_Student()
+            {
+                return "\nStudent ID:-" + this.En + "\nName:-" + Name+"\n\n";
+            }
+        }
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            
+            Student p1 = new Student(int.Parse(TextBox1.Text),TextBox2.Text ,int.Parse(TextBox3.Text),TextBox4.Text);
+            TextBox5.Text += p1.Display_Person() + p1.Display_Student();
+            TextBox1.Text = "";
+            TextBox2.Text = "";
+            TextBox3.Text = "";
+            TextBox4.Text = "";
+       
+        }
+    }
+}
