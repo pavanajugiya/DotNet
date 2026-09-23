babb4b92a26b8ac2dc72ed4c3e2db0b674fd37ba
pavan ajugiya <pavanajugiya17@gmail.com>
2026-09-23 14:00:39 +0530

    Add files via upload

---
+++ tuple_ex_23_9.aspx
@@ -0,0 +1,31 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="tuple_ex_23_9.aspx.cs" Inherits="dot_net_23_9.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            Name :
+            <asp:TextBox ID="txtname" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            Age :
+            <asp:TextBox ID="txtage" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            Email :
+            <asp:TextBox ID="txtemail" runat="server"></asp:TextBox>
+&nbsp;<br />
+            <br />
+            <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Submit" />
+            <br />
+            <br />
+            <asp:Label ID="lblshow" runat="server"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ tuple_ex_23_9.aspx.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace dot_net_23_9
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        public List<Tuple<string,int,string>>people
+        {
+            get
+            {
+                if (ViewState["people"] == null)
+                    ViewState["people"] = new List<Tuple<string,int,string>>();
+                return (List <Tuple<string, int, string>>) ViewState["people"];
+            }
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            String Name = txtname.Text;
+            int age = int.Parse(txtage.Text);
+            String email = txtemail.Text;
+
+            var p = Tuple.Create(Name, age, email);
+            people.Add(p);
+            txtname.Text = "";
+            txtage.Text = "";
+            txtemail.Text = "";
+            Display();
+            //lblshow.Text = "Name : " + p.Item1 + "<br/> Age : "  + p.Item2 + "<br/> Email : " + p.Item3;
+            
+        }
+        public void Display()
+        {
+            string result = "";
+            foreach(var p in people)
+            {
+                result += "Name : " + p.Item1 + "<br/> Age : " + p.Item2 + "<br/> Email : " + p.Item3 + "<br/>";
+     
+            }
+            lblshow.Text = result;
+        }
+    }
+}
